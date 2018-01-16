0255adbafcf9cdffbb6ebe8ef9df311f44065fd2
olivier blunt <blunt7690@gmail.com>
2018-01-16 14:00:20 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# Certbot for Nginx
+
+A simple and light docker image to create/renew SSL certificates with letsencrypt.
+
+# Howto
+
+Following sample is for nginx, but you can use it with apache too. Just change the proxy configuration.
+
+First you need to forward certbot's request to our certbot container:
+```
+# Letsencrypt
+location ^~ /.well-known/acme-challenge/ {
+	proxy_pass http://certbot:80;
+}
+```
+Host and port are to change accordingly to your configuration. I recommand using docker-compose to have the hostname mapping.
+
+Then you need to add an ssl configuration and the volume ./ssl bind to /etc/nginx/ssl/
+```
+listen 443 ssl;
+ssl_certificate /etc/nginx/ssl/fullchain.pem;
+ssl_certificate_key /etc/nginx/ssl/privkey.pem;
+```
+
+To finish, you need to have a running certbot container:
+`docker run -d -e "DOMAIN=blunt.sh" -e "EMAIL=contact@blunt.sh" -v ./ssl:/result/ssl -e "RELOAD_CMD=???" -n certbot blunt1337/certbot`
+The reload command depends on your configuration, it is run every time the ssl certificate is modified.
+Our container as docker installed in it, so if you want to execute commands on another container, you can by adding `-v /var/run/docker.sock:/var/run/docker.sock:ro`
+For example: `docker exec sample_web_1 /bin/sh -c 'kill -SIGHUP $(cat /var/run/nginx.pid)'` (don't forget to escape $ with \ when passing the command inside -e "RELOAD_CMD=\$escaped", and with $$ in compose.yml)
+
+# You can checkout the sample docker-compose
+
+Change the domain name 'home.blunt.sh' inside nginx.conf and docker-compose.yml, then run it with docker-compose up
---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+# Packages
+apk upgrade -q -U -a
+apk add --update openssl
+apk add --update certbot
+apk add --update docker
+
+# Pathes
+mkdir -p /result/ssl
+
+# Default ssl self-signed
+mkdir -p /etc/letsencrypt/selfsigned
+openssl req -new -newkey rsa:2048 -days 1 -nodes -x509 \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=sample.com" \
+    -keyout /etc/letsencrypt/selfsigned/privkey.pem \
+	-out /etc/letsencrypt/selfsigned/fullchain.pem
+
+# Main script
+echo "#!/bin/sh
+exec 2>&1
+
+# Check envs
+if [ -z \"\$EMAIL\" ]; then
+    echo \"Missing email environement variable\" >&2
+    exit 1
+fi
+if [ -z \"\$DOMAIN\" ]; then
+    echo \"Missing domain environement variable\" >&2
+    exit 1
+fi
+
+# Default ssl self-signed
+if [ ! -f /result/ssl/privkey.pem ] || [ ! -f /result/ssl/fullchain.pem ]; then
+	cp /etc/letsencrypt/selfsigned/privkey.pem /result/ssl/privkey.pem
+    cp /etc/letsencrypt/selfsigned/fullchain.pem /result/ssl/fullchain.pem
+fi
+
+# Wait 30sec for nginx to start
+sleep 30s
+
+while true; do
+    # Generate / renew
+    certbot certonly --standalone --preferred-challenges http -m \"\$EMAIL\" -d \"\$DOMAIN\" --cert-name default -n --agree-tos --keep
+    
+    # Old cert md5
+    sum=\$(md5sum /result/ssl/fullchain.pem)
+    
+    # Replace self signed / old files if needed
+    if [ \"\$sum\" != \"\$(md5sum /etc/letsencrypt/live/default/fullchain.pem)\" ]; then
+        cp -Lfu /etc/letsencrypt/live/default/fullchain.pem /result/ssl/fullchain.pem
+        cp -Lfu /etc/letsencrypt/live/default/privkey.pem /result/ssl/privkey.pem
+        
+        # Reload nginx config
+        /bin/sh -c \"\$RELOAD_CMD\"
+    fi
+    
+    # Wait to renew
+    sleep 1d
+done" > /bootstrap.sh
+chmod +x /bootstrap.sh
+
+# Clean
+rm -rf /tmp/*
+rm -rf /var/cache/apk/*
+rm -r "$0"
