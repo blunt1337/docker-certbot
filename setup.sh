71c3167ae03893e2f25a27a0582f37b3624e9af2
olivier blunt <blunt7690@gmail.com>
2018-03-26 10:11:18 +0200

    Fix tabs

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ mkdir -p /result/ssl
 # Default ssl self-signed
 mkdir -p /etc/letsencrypt/selfsigned
 openssl req -new -newkey rsa:2048 -days 1 -nodes -x509 \
-    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=sample.com" \
-    -keyout /etc/letsencrypt/selfsigned/privkey.pem \
+	-subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=sample.com" \
+	-keyout /etc/letsencrypt/selfsigned/privkey.pem \
 	-out /etc/letsencrypt/selfsigned/fullchain.pem
 
 # Main script
@@ -23,41 +23,41 @@ exec 2>&1
 
 # Check envs
 if [ -z \"\$EMAIL\" ]; then
-    echo \"Missing email environement variable\" >&2
-    exit 1
+	echo \"Missing email environement variable\" >&2
+	exit 1
 fi
 if [ -z \"\$DOMAIN\" ]; then
-    echo \"Missing domain environement variable\" >&2
-    exit 1
+	echo \"Missing domain environement variable\" >&2
+	exit 1
 fi
 
 # Default ssl self-signed
 if [ ! -f /result/ssl/privkey.pem ] || [ ! -f /result/ssl/fullchain.pem ]; then
 	cp /etc/letsencrypt/selfsigned/privkey.pem /result/ssl/privkey.pem
-    cp /etc/letsencrypt/selfsigned/fullchain.pem /result/ssl/fullchain.pem
+	cp /etc/letsencrypt/selfsigned/fullchain.pem /result/ssl/fullchain.pem
 fi
 
 # Wait 30sec for nginx to start
 sleep 30s
 
 while true; do
-    # Generate / renew
-    certbot certonly --standalone --preferred-challenges http -m \"\$EMAIL\" -d \"\$DOMAIN\" --cert-name default -n --agree-tos --keep
-    
-    # Old cert md5
-    sum=\$(md5sum /result/ssl/fullchain.pem)
-    
-    # Replace self signed / old files if needed
-    if [ \"\$sum\" != \"\$(md5sum /etc/letsencrypt/live/default/fullchain.pem)\" ]; then
-        cp -Lfu /etc/letsencrypt/live/default/fullchain.pem /result/ssl/fullchain.pem
-        cp -Lfu /etc/letsencrypt/live/default/privkey.pem /result/ssl/privkey.pem
-        
-        # Reload nginx config
-        /bin/sh -c \"\$RELOAD_CMD\"
-    fi
-    
-    # Wait to renew
-    sleep 1d
+	# Generate / renew
+	certbot certonly --standalone --preferred-challenges http -m \"\$EMAIL\" -d \"\$DOMAIN\" --cert-name default -n --agree-tos --keep
+	
+	# Old cert md5
+	sum=\$(md5sum /result/ssl/fullchain.pem)
+	
+	# Replace self signed / old files if needed
+	if [ \"\$sum\" != \"\$(md5sum /etc/letsencrypt/live/default/fullchain.pem)\" ]; then
+		cp -Lfu /etc/letsencrypt/live/default/fullchain.pem /result/ssl/fullchain.pem
+		cp -Lfu /etc/letsencrypt/live/default/privkey.pem /result/ssl/privkey.pem
+		
+		# Reload nginx config
+		/bin/sh -c \"\$RELOAD_CMD\"
+	fi
+	
+	# Wait to renew
+	sleep 1d
 done" > /bootstrap.sh
 chmod +x /bootstrap.sh
 
